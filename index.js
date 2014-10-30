9d8c9297b59a225257b2f9c9d09e756161f5788d
Vikrant Varma <vikrant.varma94@gmail.com>
2014-10-31 00:29:30 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+var xal = require('../../xal-javascript');
+var spawn = require('child_process').spawn;
+
+var presenceCertainty = 0.0;
+var present = false;
+
+var decayTime = 6000;
+
+function onPresenceDetected() {
+    if (presenceCertainty <= 0) {
+        xal.createEvent('xi.event.inform.user.presence', function(state, done) {
+            state.put('xi.event.inform.user.presence', true);
+            done(state);
+        });
+    }
+    presenceCertainty = 1.0;
+    present = true;
+}
+
+function decayCertainty() {
+    if (presenceCertainty > 0) {
+        presenceCertainty -= 0.1;
+    }
+    else {
+        xal.log.info('Absence detected');
+        if (!present) {
+            return;
+        }
+        presenceCertainty = 0;
+        present = false;
+        xal.createEvent('xi.event.inform.user.presence', function(state, done) {
+            state.put('xi.event.inform.user.presence', false);
+            done(state);
+        });
+    }
+}
+
+setInterval(decayCertainty, decayTime);
+
+xal.on('xi.event.ask.user.presence', function(state, next) {
+    state.put('xi.event.inform.user.presence', {
+        value: presenceCertainty > 0,
+        certainty: presenceCertainty
+    });
+    next(state);
+});
+
+xal.on('xi.event.input', function(state, next) {
+    xal.log.debug('Presence detected: input event');
+    onPresenceDetected();
+});
+
+
+var presence = spawn('./presence.py', [], {
+    cwd: process.cwd(),
+    env: process.env
+});
+
+presence.stdout.setEncoding('utf8');
+presence.stderr.setEncoding('utf8');
+
+presence.stdout.on('data', function(data) {
+    if (data.match(/Presence detected/)) {
+        xal.log.debug(data);
+        onPresenceDetected();
+    }
+});
+
+presence.stderr.on('data', function(data) {
+    xal.log.warn('stderr: ', data);
+});
+
+presence.on('close', function(code, signal) {
+    xal.log.info('presence.py closed');
+});
+
+xal.start({name: 'Presence'});
---
+++ presence.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import cv2
+import time
+import numpy
+import sys
+
+webcam = cv2.VideoCapture(0)
+
+frontal_face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+profile_face_cascade = cv2.CascadeClassifier('haarcascade_profileface.xml')
+upper_body_cascade = cv2.CascadeClassifier('haarcascade_upperbody.xml')
+lower_body_cascade = cv2.CascadeClassifier('haarcascade_lowerbody.xml')
+full_body_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+result, last_frame, last_frame2 = numpy.array([]), numpy.array([]), numpy.array([])
+
+while(True):
+    ret, frame = webcam.read()
+
+    frame = cv2.resize(frame, (0,0), fx=0.1, fy=0.1)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    front_faces = frontal_face_cascade.detectMultiScale(gray, 1.1, 5)
+    profile_faces = profile_face_cascade.detectMultiScale(gray, 1.1, 5)
+    #full_bodies = full_body_cascade.detectMultiScale(gray, 1.3, 5)
+    #upper_bodies = upper_body_cascade.detectMultiScale(gray, 1.3, 4)
+    #lower_bodies = lower_body_cascade.detectMultiScale(gray, 1.3, 5)
+
+    presence = False
+
+    for (x, y, w, h) in front_faces:
+        presence = True
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+
+    for (x, y, w, h) in profile_faces:
+        presence = True
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+
+    # for (x, y, w, h) in upper_bodies:
+    #     presence = True
+    #     cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+
+    # for (x, y, w, h) in full_bodies:
+    #     presence = True
+    #     cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,255), 2)
+
+    if presence:
+        print 'Presence detected: face detection'
+        sys.stdout.flush()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    if (len(sys.argv) >= 2 and sys.argv[1] == '--display'):
+        cv2.imshow('frame', frame)
+
+    if last_frame.size > 0 and last_frame2.size > 0:
+        d1 = cv2.absdiff(last_frame, gray)
+        d2 = cv2.absdiff(last_frame2, gray)
+        result = cv2.bitwise_and(d1, d2)
+        ret, result = cv2.threshold(result, 50, 255, cv2.THRESH_BINARY)
+        kernel_ones = numpy.ones((3,3), numpy.uint8)
+        eroded = cv2.erode(result, kernel_ones, iterations=1)
+        dilated = cv2.dilate(eroded, kernel_ones, iterations=1)
+
+        changed_pixels = cv2.countNonZero(dilated)
+        changed_pixels_frac = float(changed_pixels) / float(result.size)
+        if changed_pixels_frac > 0.005:
+            print 'Presence detected: motion detection'
+            sys.stdout.flush()
+
+        if (len(sys.argv) >= 2 and sys.argv[1] == '--display'):
+            cv2.imshow('motion diff', result)
+            cv2.imshow('motion diff - eroded', eroded)
+            cv2.imshow('motion diff - dilated', dilated)
+
+    last_frame = last_frame2;
+    last_frame2 = gray;
+
+    time.sleep(0.6)
